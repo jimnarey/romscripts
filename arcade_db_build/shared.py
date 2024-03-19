6f12eeba75446e21a62ff29216428ddc717427cd
Jim Narey <jimnarey@me.com>
2024-03-19 02:37:04 +0000

    Create db shared module

---
+++ arcade_db_build/shared.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import bz2
+import xml.etree.ElementTree as ET
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+MAME_SOURCES = os.path.join(SCRIPT_PATH, "mame_db_source", "dats")
+FBA_SOURCES = os.path.join(SCRIPT_PATH, "fba_db_source", "dats")
+MAME_DATS = [os.path.join(MAME_SOURCES, file) for file in os.listdir(MAME_SOURCES)]
+
+
+def get_source_contents(path: str) -> str:
+    with bz2.open(path, "r") as bzip_file:
+        return bzip_file.read().decode("utf-8")
+
+
+def get_source_root(dat_contents: str) -> ET.Element:
+    root = ET.fromstring(dat_contents)
+    return root
