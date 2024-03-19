eeacaaab28afe8082e6f4315eb57ca45238de2b7
Jim Narey <jimnarey@me.com>
2024-03-19 22:02:48 +0000

    Validate tag name consistencies
    Update README.md with details of tag name changes across MAME
    versions.

--- arcade_db_build/mame_db_source/README.md
+++ arcade_db_build/mame_db_source/README.md
@@ -7,3 +7,9 @@ Some archives have both DAT and XML, some (approx 53 - 84, in rar format, just D
 BZIP  was used as it has the best compression ratio for text-based files. It's slow to compress but not much slower than other formats for decompression.
 
 Compressed, the collection of DAT/XML files is 1.6GB. Uncompressed it's 26.8GB.
+
+When MAME and MESS became a single emulator (release 0.162  - May, 2015), they changed how they store the data.  But only for what they call the rom file. I assume this was to create a better distinction between softlists (MESS) and arcade (MAME).
+
+Prior to 0.162, individual arcade rom information/data was stored under "game".  Starting with release 0.162 they are stored under "machine".
+
+So with 0.159, the Mame.xml would have its data listed under "game", and the importer is looking for "machine".
--- arcade_db_build/validate_sources.py
+++ arcade_db_build/validate_sources.py
@@ -6,7 +6,27 @@ For now, this only validates the MAME source files
 
 import os
 import multiprocessing
+import xml.etree.ElementTree as ET
 import shared
+
+
+def validate_root_tag(root: ET.Element) -> None:
+    if root.tag not in ["mame", "datafile"]:
+        print("Unrecognised root tag: ", root)
+
+
+def validate_num_headers(path: str, element_tags: list[str]) -> None:
+    if element_tags.count("header") > 1:
+        print("Multiple header elements: ", path)
+
+
+def validate_tag_names(path: str, element_tags: list[str]) -> None:
+    element_tags_no_header = [element_tag for element_tag in element_tags if element_tag != "header"]
+    element_tags_set = set(element_tags_no_header)
+    if len(element_tags_set) > 1:
+        print("Multiple tag types: ", path, element_tags_set)
+    if et := element_tags_set.pop() not in ("game", "machine"):
+        print("Unrecognised element tag: ", path, et)
 
 
 def process_dat(path: str) -> None:
@@ -16,8 +36,11 @@ def process_dat(path: str) -> None:
         root = shared.get_source_root(source)
     except Exception as e:
         print("Error: ", type(e), path)
-    if root and root.tag not in ["mame", "datafile"]:
-        print("Unrecognised root tag: ", path, root)
+    if root:
+        validate_root_tag(root)
+        element_tags = [element.tag for element in root]
+        validate_num_headers(path, element_tags)
+        validate_tag_names(path, element_tags)
 
 
 def process_files():
@@ -27,3 +50,7 @@ def process_files():
 
 if __name__ == "__main__":
     process_files()
+    # df_dat_path = "/home/jimnarey/projects/romscripts/arcade_db_build/mame_db_source/dats/MAME 0.53.dat.bz2"
+    # m_dat_path = "/home/jimnarey/projects/romscripts/arcade_db_build/mame_db_source/dats/MAME 0.141.xml.bz2"
+    # d = shared.get_source_root(shared.get_source_contents(df_dat_path))
+    # m = shared.get_source_root(shared.get_source_contents(m_dat_path))
