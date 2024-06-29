65d87011cc6322cb6e294657d792795ccf23e69e
Jim Narey <jimnarey@me.com>
2024-06-29 23:35:36 +0100

    Limit get_sub_elements cache size

--- arcade_db_build/shared/utils.py
+++ arcade_db_build/shared/utils.py
@@ -4,6 +4,6 @@ from lxml import etree as ET
 import functools
 
 
-@functools.cache
+@functools.lru_cache(maxsize=10)
 def get_sub_elements(parent_element: ET._Element, tag_name: str) -> list[ET._Element]:
     return parent_element.findall(tag_name)
